09dbcfb17e5a6eca328985a97033e3e43c483e45
ella 🐰 <ella@isy.cat>
2026-02-19 16:38:33 +0100

    Clean up gradle build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     `maven-publish`
 }
 
-group = "com.isycat.dotahalp"
-version = "1.0.0"
+group = "com.github.isycat"
+version = "v1.0.3"
 
 val ktorVersion = "3.0.0"
 val coroutinesVersion = "1.8.1"
@@ -19,19 +19,19 @@ repositories {
 dependencies {
     // For local development in composite build
     implementation("com.github.isycat:steam-utils:v1.0.0")
-    
+
     // Ktor server for GSI
     api("io.ktor:ktor-server-core:$ktorVersion")
     api("io.ktor:ktor-server-cio:$ktorVersion")
     api("io.ktor:ktor-server-content-negotiation:$ktorVersion")
     api("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
-    
+
     // Serialization
     api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-    
+
     // Coroutines
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-    
+
     testImplementation(kotlin("test"))
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
     testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
@@ -56,35 +56,35 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.github.isycat"
             artifactId = "gsi-core"
-            version = "1.0.0"
-            
+            version = "v1.0.3"
+
             from(components["java"])
-            
+
             pom {
                 name.set("GSI Core")
                 description.set("Generic Game State Integration framework for any Source engine game")
                 url.set("https://github.com/isycat/gsi-core")
-                
+
                 licenses {
                     license {
                         name.set("MIT License")
                         url.set("https://opensource.org/licenses/MIT")
                     }
                 }
-                
+
                 developers {
                     developer {
                         id.set("isycat")
                         name.set("isycat")
                     }
                 }
-                
+
                 scm {
                     connection.set("scm:git:git://github.com/isycat/gsi-core.git")
                     developerConnection.set("scm:git:ssh://github.com/isycat/gsi-core.git")
                     url.set("https://github.com/isycat/gsi-core")
                 }
-                
+
             }
         }
     }
